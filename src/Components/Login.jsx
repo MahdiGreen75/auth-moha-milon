75c1049c234312e5d78600bbc8973846e584427a
Ricky Ponting <qarimahdi217@gmail.com>
2023-10-03 04:52:50 +0100

    Add and style  login and registration route page.

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,10 +1,50 @@
-
+import { Link } from "react-router-dom";
 
 const Login = () => {
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        console.log(email, password);
+    }
+
     return (
-        <div>
-            
+
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <form onSubmit={handleLogin}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button type="submit" className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                        <p>New here. Please <Link to="/register"><a className="text-blue-600">Register</a></Link></p>
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
 };
 
